cc3fdc51d208a4543e64c4d4d1a2b8f5470a40e6
m.saadat <m.saadat@hasintech.com>
2021-10-02 14:38:55 +0330

    bug fix

--- src/LyfoesSolver.Application/Program.cs
+++ src/LyfoesSolver.Application/Program.cs
@@ -7,7 +7,7 @@ namespace LyfoesSolver.Application
 	{
 		static void Main(string[] args)
 		{
-            var game = GetUltraGame();
+            var game = GetUltraGame17();
 
             IGameQueue queue = new PriorityGameQueue();
             SolveGame(game, queue);
@@ -22,7 +22,7 @@ namespace LyfoesSolver.Application
             var start = DateTime.Now;
             var res = Solver.SolveBFS(game, gameQueue);
             var time = (DateTime.Now - start).TotalMilliseconds;
-            Console.WriteLine($"with method {nameof(gameQueue)} solved in {time} milliseconds. {Environment.NewLine} visited states : {res.CheckedGames}.{Environment.NewLine} Moves {res.Moves.Sum(x => x.Count)} {Environment.NewLine} Depth : {res.Depth} ");
+            Console.WriteLine($"with method {gameQueue.GetType().Name} solved in {time} milliseconds. {Environment.NewLine} visited states : {res.CheckedGames}.{Environment.NewLine} Moves {res.Moves.Sum(x => x.Count)} {Environment.NewLine} Depth : {res.Depth} ");
             Console.WriteLine($"{string.Join(Environment.NewLine, res.Moves)} ");
         }
 
@@ -44,6 +44,29 @@ namespace LyfoesSolver.Application
             game.AddPipe(13, "green", "darkgreen", "darkgreen", "black");
             game.AddPipe(14, "red", "black", "orange", "lightpink");
             game.AddPipe(15, "darkblue", "lightpink", "gray", "green");
+            game.AddPipe(16);
+            game.AddPipe(17);
+            return game;
+        }
+        
+        static Game GetUltraGame17()
+		{
+            var game = new Game(4);
+            game.AddPipe(1, "lightblue", "darkgreen", "lightpink", "darkblue");
+            game.AddPipe(2, "orange", "yellow", "pink", "lightgreen");
+            game.AddPipe(3, "lightblue", "darkgreen", "grey", "lightpink");
+            game.AddPipe(4, "darkgreen", "green", "orange", "blue");
+            game.AddPipe(5, "orange", "red", "lightgreen", "white");
+            game.AddPipe(6, "lightpink", "yellow", "grey", "blue");
+            game.AddPipe(7, "pink", "lightpink", "pink", "black");
+            game.AddPipe(8, "purple", "blue", "grey", "darkblue");
+            game.AddPipe(9, "red", "purple", "lightgreen", "red");
+            game.AddPipe(10, "black", "purple", "black", "black");
+            game.AddPipe(11, "green", "green", "white", "yellow");
+            game.AddPipe(12, "red", "purple", "darkblue", "lightblue");
+            game.AddPipe(13, "grey", "darkgreen", "pink", "orange");
+            game.AddPipe(14, "white", "white", "lightblue", "yellow");
+            game.AddPipe(15, "darkblue", "lightgreen", "green", "blue");
             game.AddPipe(16);
             game.AddPipe(17);
             return game;
